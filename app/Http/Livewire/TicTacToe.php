9264a58fb0b6d0e80e6dd19cf2d3aadf3ea7077c
Rheanne McIntosh <rheanne.mcintosh@visualsoft.co.uk>
2023-06-13 03:04:07 +0100

    Make blank Livewire TicTacToe component

---
+++ app/Http/Livewire/TicTacToe.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class TicTacToe extends Component
+{
+    public function render()
+    {
+        return view('livewire.tic-tac-toe');
+    }
+}
